062bbd27641c7eb448dfdce6540961daee30f9a3
Magdalena Szymczak <mwozniak.kontakt@gmail.com>
2018-01-08 17:03:01 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var a = prompt("Enter value a of triangle");
+var h = prompt("Enter value h of triangle");
+var triangleArea = a*h/2;
+alert(triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+
+
